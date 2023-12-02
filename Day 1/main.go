112c9a22f2b09e81fdbfdf35c019e8f9d0f23c9e
KonradowyPL <konrad.chrzaszczak@gmail.com>
2023-12-02 10:25:58 +0100

    Day 1 🎄

--- Day 1/main.go
+++ Day 1/main.go
@@ -14,7 +14,8 @@ func main() {
 		log.Fatal(err)
 	}
 	var lines []string = strings.Split(string(content), "\r\n")
-	var sum int = 0
+	var suma int = 0
+	var sumb int = 0
 
 	dict := []string{
 		"one",
@@ -30,29 +31,36 @@ func main() {
 
 	for _, line := range lines {
 
-		var digits []rune
+		var digitsa []rune
+		var digitsb []rune
+
 		var digit []rune
 
 		for _, char := range line {
 			if 57 >= char && char >= 48 {
-				digits = append(digits, char)
+				digitsa = append(digitsa, char)
+				digitsb = append(digitsb, char)
 
 			} else {
 				digit = append(digit, char)
 				for index, item := range dict {
 
 					if strings.HasSuffix(string(digit), item) {
-						digits = append(digits, rune(strconv.Itoa(index + 1)[0]))
+						digitsb = append(digitsb, rune(strconv.Itoa(index + 1)[0]))
 					}
 				}
 			}
 		}
+		var spliteda string = string(digitsa[0]) + string(digitsa[len(digitsa)-1])
+		var splitedb string = string(digitsb[0]) + string(digitsb[len(digitsb)-1])
+		valuea, _ := strconv.Atoi(spliteda)
+		valueb, _ := strconv.Atoi(splitedb)
 
-		var splited string = string(digits[0]) + string(digits[len(digits)-1])
-		value, _ := strconv.Atoi(splited)
-
-		sum += value
+		suma += valuea
+		sumb += valueb
 	}
 
-	fmt.Println(sum)
+	fmt.Println("Sum a:", suma)
+	fmt.Println("Sum b:", sumb)
+
 }
